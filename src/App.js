514bfa0448d7d3230149fcb6bdd86a5e3929a5d3
Rob-Swensen <rg.swensen801@gmail.com>
2020-03-05 15:36:32 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Finder from './Components/Finder';
+import Pokedex from './Components/Pokedex';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+       caughtPokemon: [],
+    }
+    this.catchPokemon = this.catchPokemon.bind(this)
+    this.saveName = this.saveName.bind(this)
+    this.releasePokemon = this.releasePokemon.bind(this)
+  }
+
+  componentDidMount(){
+    axios.get('/api/pokemon')
+    .then(response => {
+      this.setState({
+        caughtPokemon: response.data
+      })
+  })
+  }
+
+  catchPokemon(pokemon){
+    axios.post('/api/pokemon', {pokemon})
+    .then(response => {
+      this.setState({
+        caughtPokemon: response.data
+      })
+    })
+  }
+
+  saveName(id, newName){
+    axios.put(`/api/pokemon/${id}`, {name: newName})
+    .then(response => {
+      this.setState({
+        caughtPokemon: response.data
+      })
+    })
+  }
+
+  releasePokemon(id){
+    axios.delete(`/api/pokemon/${id}`)
+    .then(response => {
+      this.setState({
+        caughtPokemon: response.data
+      })
+    })
+  }
+
+  render(){
+    return(
+      <div className='App'>
+        <Header />
+        <Finder catchPokemon = {this.catchPokemon} />
+        <Pokedex 
+          releasePokemon = {this.releasePokemon}
+          saveName = {this.saveName}
+          caughtPokemon = {this.state.caughtPokemon}
+        /> 
+      </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/Components/Finder.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import Grass from './Grass';
+import axios from 'axios';
+
+class Finder extends Component {
+    constructor(){
+        super();
+
+            this.state = {
+                wildPokemon: []
+            }
+
+            this.componentDidMount = this.componentDidMount.bind(this)
+    }
+
+    componentDidMount(){
+        axios.get('/api/wild-pokemon')
+        .then(response => {
+            this.setState({wildPokemon: response.data})
+        })
+    }
+
+    render(){
+        const pokemonList = this.state.wildPokemon.map(element => {
+            return <Grass 
+            key={element.id} 
+            pokemon={element} 
+            catchPokemon = {this.props.catchPokemon}
+            refreshFn={this.componentDidMount}/>
+        })
+        return(
+            <div className='finder-box'>
+                {pokemonList}
+            </div>
+        )
+    }
+}
+
+export default Finder
